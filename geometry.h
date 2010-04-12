5e0fd2f7e4a500ce021fae9305720787b2614666
Aaron Beitch <aaron.beitch@gmail.com>
2010-04-12 16:09:14 -0700

    Added shape class.

--- geometry.h
+++ geometry.h
@@ -8,15 +8,22 @@
 #include "algebra3.h"
 
 class ray;
+class shape;
 
 class ray
 {
- public:
+public:
   ray();
   ray(const vec3 p, const vec3 d);
  
   vec3 pos;
   vec3 dir;
+}
+
+class shape
+{
+public:
+  virtual bool intersect(ray r);
 }
 
 #endif
