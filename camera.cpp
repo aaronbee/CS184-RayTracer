c5effbcb6f1edbdf4babff3e5b2f557887f8a2c5
Aaron Beitch <aaron.beitch@gmail.com>
2010-04-20 21:19:02 -0700

    camera working?

--- camera.cpp
+++ camera.cpp
@@ -23,12 +23,13 @@ Ray Camera::generateRay(vec2 pixel)
   double fovXRad = fovx * M_PI / 180.0;
   double fovYRad = fovy * M_PI / 180.0;
   
-  alpha = tan(fovXRad / 2) * (pixel[0] - (width / 2)) / (width / 2);
+  alpha = - tan(fovXRad / 2) * (pixel[0] - (width / 2)) / (width / 2);
   beta = tan(fovYRad / 2) * ((height / 2) - pixel[1]) / (height / 2);
 
   dir = (alpha * u) + (beta * v) - w;
   dir.normalize();
-  dir += scene->getCameraPos();
+  //  dir += scene->getCameraPos();
+  //  dir.normalize();
 
   Ray rtn = Ray(scene->getCameraPos(), dir);
 
