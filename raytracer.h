edb690bea7ed933e768271d956b5a6a6584bfc82
Aaron Beitch <aaron.beitch@gmail.com>
2010-04-21 22:57:24 -0700

    Transformations working?

--- raytracer.h
+++ raytracer.h
@@ -35,7 +35,7 @@ public:
 	// Last argument says to drop w
 	// if buggy try dealing with homogeneity thing.
 	p = vec3(m * vec4(pos, 1));
-	d = vec3(m * vec4(dir, 0), 3);
+	d = vec3(m * vec4(dir, 0), 3).normalize();
 	return Ray(p, d);
   }
 
